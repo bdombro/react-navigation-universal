aa3f36915cec171f102375f998a041ad284fbacf
Brian <bdombro@gmail.com>
2020-01-20 19:28:04 -0500

    Removed bug workaround for expo build

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = async function(env, argv) {
-  /**
-   * There is a bug in expo-cli that prevents prodenv from being set.
-   * Ref: https://github.com/expo/expo-cli/issues/919
-   *
-   * The following will force webpack to bundle in production mode
-   */
-  if (process.env.BUILD_ENV === 'production') {
-    env.mode = 'production';
-    env.development = false;
-    env.production = true;
-  }
 
   const config = await expoConfig(env, argv);
 
